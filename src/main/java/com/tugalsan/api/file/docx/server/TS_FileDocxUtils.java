2b38a2fe29e8a56408380803826baa9927a6d0a5
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-24 12:28:31 +0300

    Update TS_FileDocxUtils.java

--- src/main/java/com/tugalsan/api/file/docx/server/TS_FileDocxUtils.java
+++ src/main/java/com/tugalsan/api/file/docx/server/TS_FileDocxUtils.java
@@ -1,6 +1,6 @@
 package com.tugalsan.api.file.docx.server;
 
-import com.tugalsan.api.charset.client.TGS_CharSetCast;
+import com.tugalsan.api.charset.client.TGS_CharSet;
 import java.io.*;
 import java.math.*;
 import java.nio.file.*;
@@ -159,7 +159,7 @@ public class TS_FileDocxUtils implements AutoCloseable {
             }
 
             Integer format = null;
-            var imgFileStrLC = TGS_CharSetCast.toLocaleLowerCase(imgFileStr);
+            var imgFileStrLC = TGS_CharSet.cmn().languageDefault().toLowerCase(imgFileStr);
             if (imgFileStr.endsWith(".emf")) {
                 format = XWPFDocument.PICTURE_TYPE_EMF;
             } else if (imgFileStrLC.endsWith(".wmf") || imgFileStrLC.endsWith(".wmf")) {
